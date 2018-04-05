9a6bac92aeee5a054956d9ee85d8fd948a322594
dopemo <mkhalifa@csumb.edu>
2018-04-04 19:13:24 -0700

    last commit

--- legacy_router.py
+++ legacy_router.py
@@ -8,22 +8,21 @@ from mininet.log import setLogLevel, info
 
 def myNetwork():
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+	defaultIP='10.0.1.1/24'
+    net = Mininet( topo=None,build=False)
 
     info( '*** Adding controller\n' )
     info( '*** Add switches\n')
-    r1./ = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1= net.addHost('r0', cls=Node, ip=defaultIP)
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h2 = net.addHost('h2', cls=Host, ip='127.0.0.2', defaultRoute=None)
-    h1 = net.addHost('h1', cls=Host, ip='127.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.100/24', defaultRoute='10.0.1.1')
+    h1 = net.addHost('h1', cls=Host, ip='172.16.0.100/12', defaultRoute='via 172.16.0.1')
 
     info( '*** Add links\n')
-    net.addLink(h1, r1)
-    net.addLink(h2, r1)
+    net.addLink(h1, r1,intfName2='r0-eth1', params2={'ip' : '10.0.1.1/24'})
+    net.addLink(h2, r1,intfName2='r0-eth2', params2={'ip' : '172.16.0.1/12'})
 
     info( '*** Starting network\n')
     net.build()
